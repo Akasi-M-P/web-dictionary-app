64a5820fc066abc770c06d850dd9d4c05bf7d254
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-10-13 13:44:36 +0000

    worked on word meaning

--- src/WordBody/WordBody.jsx
+++ src/WordBody/WordBody.jsx
@@ -20,6 +20,28 @@ const WordBody = () => {
           <hr className="w-full" />
         </div>
       </section>
+      <section className="w-11/12">
+        <div className="">
+          <p className="text-gray-600">Meaning</p>
+        </div>
+        <div className="w-10/12 mx-auto">
+          <ul className="list-disc text-sm">
+            <li>
+              (etc.) A set of keys used to operate a typewriter, computer etc.
+            </li>
+            <li>
+              A component of many instruments including the piano, organ, and
+              harpsichord consisting of usually black and white keys that cause
+              different tones to be produced when struck.
+            </li>
+            <li>
+              A device with keys of a musical keyboard, used to control
+              electronic sound-producing devices which may be built into or
+              separate from the keyboard device.
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 };
