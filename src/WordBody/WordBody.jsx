37c5c38430833cb48a9ba7d44b43fff73e451ddd
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-10-13 13:53:46 +0000

    update

--- src/WordBody/WordBody.jsx
+++ src/WordBody/WordBody.jsx
@@ -25,16 +25,16 @@ const WordBody = () => {
           <p className="text-gray-600">Meaning</p>
         </div>
         <div className="w-11/12 mx-auto px-2 py-4">
-          <ul className="list-disc text-sm">
-            <li>
+          <ul className="list-disc text-sm leading-6">
+            <li className="mb-4">
               (etc.) A set of keys used to operate a typewriter, computer etc.
             </li>
-            <li>
+            <li className="mb-4">
               A component of many instruments including the piano, organ, and
               harpsichord consisting of usually black and white keys that cause
               different tones to be produced when struck.
             </li>
-            <li>
+            <li className="mb-4">
               A device with keys of a musical keyboard, used to control
               electronic sound-producing devices which may be built into or
               separate from the keyboard device.
