dbf87a91dc105d3a448eef87b9a64ceb2b5e76b7
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-10-18 22:32:59 +0000

    update

--- src/WordContext/WordContext.jsx
+++ src/WordContext/WordContext.jsx
@@ -1,55 +1,3 @@
-// /* eslint-disable react/prop-types */
-// /* eslint-disable no-unused-vars */
-// import { createContext, useState } from "react";
-
-// export const WordContext = createContext();
-
-// export const WordProvider = ({ children }) => {
-//   const [wordDefinition, setWordDefinition] = useState(null);
-//   const [searchWord, setSearchWord] = useState(false);
-//   const [word, setWord] = useState("");
-
-//   const wordResult = (data) => {
-//     setWordDefinition(data);
-//     setSearchWord(true);
-//   };
-
-//   const handleSearchWord = async () => {
-//     try {
-//       const response = await fetch(
-//         `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-//       );
-//       if (response.ok) {
-//         const definedWord = await response.json();
-//         wordResult(definedWord);
-//         console.log(definedWord);
-//       } else {
-//         console.error("Failed to get word definition");
-//       }
-//     } catch (error) {
-//       console.error("Failed to get word definition", error);
-//     } finally {
-//       setWord("");
-//     }
-//   };
-
-//   return (
-//     <WordContext.Provider
-//       value={{
-//         definedWord,
-//         setWord,
-//         setWordDefinition,
-//         wordResult,
-//         wordDefinition,
-//         setSearchWord,
-//         handleSearchWord,
-//       }}
-//     >
-//       {children}
-//     </WordContext.Provider>
-//   );
-// };
-
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { createContext, useState } from "react";
@@ -60,11 +8,6 @@ export const WordProvider = ({ children }) => {
   const [wordDefinition, setWordDefinition] = useState(null);
   const [searchWord, setSearchWord] = useState(false);
   const [word, setWord] = useState("");
-
-  // const wordResult = (data) => {
-  //   setWordDefinition(data);
-  //   setSearchWord(true);
-  // };
 
   const handleSearchWord = async () => {
     try {
